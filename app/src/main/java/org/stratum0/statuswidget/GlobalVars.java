b8a57ce5195b457574983cd97255e49317b70a09
Steffen Arntz <steffen.arntz@belnet.de>
2015-02-23 09:16:20 +0100

    import of existing code/files

---
+++ app/src/main/java/org/stratum0/statuswidget/GlobalVars.java
@@ -0,0 +1,10 @@
+package org.stratum0.statuswidget;
+
+/**
+ * Created by Matthias Uschok <dev@uschok.de> on 2013-09-29.
+ */
+public class GlobalVars {
+    public static final String TAG = "Stratum0";
+    public static final String statusUrl = "http://stratumspherestatuswidget.stratum0.net:8766";
+    public static int[] appWidgetIds;
+}
